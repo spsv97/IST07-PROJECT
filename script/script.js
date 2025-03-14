1a63c659209e2727a5f4a72b885806e236af5af8
SputnikTH <rachata9866@gmail.com>
2025-03-14 12:53:12 -0700

    html form and toggling visible sections

--- script/script.js
+++ script/script.js
@@ -1,1 +1,38 @@
-// hello world
+let person = {};
+let secondPerson = {};
+let compatibilityMatrix = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    const btnPersonal = document.getElementById('btnPersonal');
+    const btnCompatibility = document.getElementById('btnCompatibility');
+    const personalSection = document.getElementById('personalSection');
+    const compatibilitySection = document.getElementById('compatibilitySection');
+})
+
+
+personalSection.style.display = 'block';
+compatibilitySection.style.display = 'none';
+
+if (btnPersonal && btnCompatibility && personalSection && compatibilitySection) {
+    btnPersonal.addEventListener('click', function () {
+        btnPersonal.classList.add('active');
+        btnCompatibility.classList.remove('active');
+        personalSection.style.display = 'block';
+        compatibilitySection.style.display = 'none';
+        compatibilitySection.classList.add('display-none');
+    });
+    btnCompatibility.addEventListener('click', function () {
+        btnCompatibility.classList.add('active');
+        btnPersonal.classList.remove('active');
+        compatibilitySection.style.display = 'block';
+        compatibilitySection.classList.remove('display-none');
+        personalSection.style.display = 'none';
+        personalSection.classList.add('display-none');
+    })
+}
+
+if (personalSection) {
+    personalSection.style.display = 'block';
+    personalSection.classList.remove('display-none');
+
+}
